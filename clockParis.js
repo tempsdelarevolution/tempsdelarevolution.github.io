96c29c9746e4f78c5ab431348a3c697ca13d2166
mayhumst <mayahumston@gmail.com>
2023-07-09 15:19:56 -0400

    Major bug fixes; reorder elements, new function for labels;

--- clockParis.js
+++ clockParis.js
@@ -6,12 +6,17 @@ const secondHand2 = document.querySelector('.second-hand-2');
 const minsHand2 = document.querySelector('.min-hand-2');
 const hourHand2 = document.querySelector('.hour-hand-2');
     
-function setRealDate() {
+function setRealTime() {
     //WHAT TIME IS IT????
     const now = new Date();
-
     //get seconds
     const seconds = now.getUTCSeconds();
+    //get minutes
+    const mins = now.getUTCMinutes();
+    //get hours
+    const hour = (now.getUTCHours() + 2) % 24;
+
+
     //change second hand
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand1.style.transform = `rotate(${secondsDegrees}deg)`;
@@ -24,9 +29,6 @@ function setRealDate() {
         digSec.innerHTML = seconds;
     }
     
-
-    //get minutes
-    const mins = now.getMinutes();
     //change minute hand
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
     minsHand1.style.transform = `rotate(${minsDegrees}deg)`;
@@ -39,8 +41,6 @@ function setRealDate() {
         digMin.innerHTML = mins;
     }
 
-    //get hours
-    const hour = (now.getUTCHours() + 2) % 24;
     //change hour hand
     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
     hourHand1.style.transform = `rotate(${hourDegrees}deg)`;
@@ -53,140 +53,103 @@ function setRealDate() {
         digHour.innerHTML = hour;
     }
 
-    //MODERN CLOCK
+    changeRealTimeLabels(now, hour);
+}
 
+function setRevTime() {
+    //WHAT TIME IS IT????
+    const now = new Date();
+
+    //get seconds
+    const seconds = now.getUTCSeconds();
+    //get minutes
+    const mins = now.getUTCMinutes();
+    //get hours
+    const hour = (now.getUTCHours() + 2) % 24;
+
+    //SECONDS ELAPSED SINCE MIDNIGHT
+    let currTime = seconds + (60 * mins) + (3600 * hour);
+
+    //get hours
+    let percentage = currTime/86400;
+    let hourDeg2 = (360 * percentage) + 90;
+    let hours2 = 10*percentage;
+    //change hour hand
+    hourHand2.style.transform = `rotate(${hourDeg2}deg)`;
+    //update hour digital
+    let dig2Hour = document.getElementById('dig-2-hour')
+    if (hours2<10) {
+        dig2Hour.innerHTML = '0' + Math.floor(hours2); //append zero if single digit
+    }
+    else {
+        dig2Hour.innerHTML = Math.floor(hours2);
+    }
+
+    //get mins
+    let minPercentage = hours2%1;
+    let mins2 = minPercentage * 100;
+    let minDeg2 = (minPercentage * 360) + 90;
+    //update min hand
+    minsHand2.style.transform = `rotate(${minDeg2}deg)`;
+    //update min digital
+    let dig2Min = document.getElementById('dig-2-min')
+    if (mins2<10) {
+        dig2Min.innerHTML = '0' + Math.floor(mins2); //append zero if single digit
+    }
+    else {
+        dig2Min.innerHTML = Math.floor(mins2);
+    }
+
+    //get secs
+    let secPercentage = mins2%1;
+    let secs2 = secPercentage*100;
+    let secDeg2 = (secPercentage * 360) + 90;
+    //update sec hand
+    secondHand2.style.transform = `rotate(${secDeg2}deg)`;
+    //update sec digital
+    let dig2Sec = document.getElementById('dig-2-sec')
+    if (secs2<10) {
+        dig2Sec.innerHTML = '0' + Math.floor(secs2); //append zero if single digit
+    }
+    else {
+        dig2Sec.innerHTML = Math.floor(secs2);
+    }
+}
+
+function changeRealTimeLabels(now, hour) {
+    /*
+        CHANGE LABELS: Called every second from real time clock
+        ***Note: potential optimization -- only called @ beginning of each day?
+
+        Will set the month, day, and weekday in french and english for REAL TIME clock
+        ***IF between September x and 23rd (festival week), 
+            call special label function
+        ELSE call regular revolutionary time
+    */
+
+
+    //MODERN CLOCK LABEL
+
+    //Declare necessary variables
+    //year
+    let currYear = now.getUTCFullYear;
     //weekday
     let currWeekdayNumber = now.getUTCDay();
     let weekdayFR = document.getElementById("weekday-modern-fr");
     let weekdayENG = document.getElementById("weekday-modern-eng");
-
-    if (hour <2) {currWeekdayNumber ++}
-    let currWeekday;
-    let currWeekdayFR;
-
-    if(currWeekdayNumber==1) {
-        currWeekday = 'Monday';
-        currWeekdayFR = "Lundi";
-    }
-    else if (currWeekdayNumber==2) {
-        currWeekday = 'Tuesday';
-        currWeekdayFR = "Mardi";
-    }
-    else if (currWeekdayNumber==3) {
-        currWeekday = 'Wednesday';
-        currWeekdayFR = "Mercredi";
-    }
-    else if (currWeekdayNumber==4) {
-        currWeekday = 'Thursday';
-        currWeekdayFR = "Jeudi";
-    }
-    else if (currWeekdayNumber==5) {
-        currWeekday = 'Friday';
-        currWeekdayFR = "Vendredi";
-    }
-    else if (currWeekdayNumber==6) {
-        currWeekday = 'Saturday';
-        currWeekdayFR = "Samedi";
-    }
-    else {
-        currWeekday = 'Sunday';
-        currWeekdayFR = "Dimanche";
-    }
-    weekdayENG.innerHTML = currWeekday + ', ';
-    weekdayFR.innerHTML = currWeekdayFR + ', '
-
     //day of month
     let currDayNumber = now.getUTCDate();
     let dayENG = document.getElementById("day-modern-eng");
     let dayFR = document.getElementById("day-modern-fr");
-
-    if(hour<2) {currDayNumber ++}; //timezone danger zone
-
-    dayENG.innerHTML = currDayNumber;
-    dayFR.innerHTML = currDayNumber;
-
-
     //month
     let currMonthNumber = now.getUTCMonth();
     let monthENG = document.getElementById("month-modern-eng");
     let monthFR = document.getElementById("month-modern-fr");
 
-    if(hour<2) {currMonthNumber ++};
-    let currMonthName;
-    let currMonthNameFR;
 
-    if(currMonthNumber == 0) {
-        currMonthName = 'January';
-        currMonthNameFR = "Janvier";
-    }
-    else if(currMonthNumber == 1) {
-        currMonthName = 'February';
-        currMonthNameFR = "Fevrier";
-    }
-    else if(currMonthNumber == 2) {
-        currMonthName = 'March';
-        currMonthNameFR = "Mars";
-    }
-    else if(currMonthNumber == 3) {
-        currMonthName = 'April';
-        currMonthNameFR = "Avril";
-    }
-    else if(currMonthNumber == 4) {
-        currMonthName = 'May';
-        currMonthNameFR = "Mai";
-    }
-    else if(currMonthNumber == 5) {
-        currMonthName = 'June';
-        currMonthNameFR = "Juin";
-    }
-    else if(currMonthNumber == 6) {
-        currMonthName = 'July';
-        currMonthNameFR = "Juillet";
-    }
-    else if(currMonthNumber == 7) {
-        currMonthName = 'August';
-        currMonthNameFR = "Aout";
-    }
-    else if(currMonthNumber == 8) {
-        currMonthName = 'September';
-        currMonthNameFR = "Septembre";
-    }
-    else if(currMonthNumber == 9) {
-        currMonthName = 'October';
-        currMonthNameFR = "Octobre";
-    }
-    else if(currMonthNumber == 10) {
-        currMonthName = 'November';
-        currMonthNameFR = "Novembre";
-    }
-    else {
-        currMonthName = 'December';
-        currMonthNameFR = "Decembre";
-    }
-
-    monthENG.innerHTML = currMonthName + ' ';
-    monthFR.innerHTML = ' ' + currMonthNameFR;
-    
-
-    //
-    //
-    //  REVOLUTIONARY DATE
-    //
-    //
-
-    //necessary variables
-    let revMonthENG;
-    let revMonthFR;
-    let revWeekdayENG;
-    let revWeekdayFR;
-    let revDayENG;
-    let revDayFR;
-    let revSeason;
-    let revDayNumber;
-    let revDayNumberINMONTH;
-    
-    let currYear = now.getUTCFullYear;
-    if(hour <2) { //danger zone for paris time
+    //TIMEZONE DANGER ZONE: YEAR
+    // increment year IF december 31st between 10 pm and midnight (0 and 2am in paris)
+    if(hour < 2 && currMonthNumber == 11 && currDayNumber == 31 && currMonthNumber == 11) { 
         currYear ++;
     }
 
@@ -198,6 +161,186 @@ function setRealDate() {
     else {
         isLeapYear = false;
     }
+
+    // IS IT END OF MONTH ?? TIMEZONE DANGER ZONE: CONVERTING UTC TO PARIS
+    let endOfMonth = false;
+    if (currMonthNumber == 0 || currMonthNumber == 2 || currMonthNumber == 4 || 
+        currMonthNumber == 6 || currMonthNumber == 7 || currMonthNumber == 9 || currMonthNumber == 11) 
+        { //month with 31 days
+            if (currDayNumber == 31) {
+                endOfMonth = true;
+            }
+            else { //redundant case but just in case
+                endOfMonth = false;
+            }
+        }
+    else if (currMonthNumber == 3 || currMonthNumber == 5 || currMonthNumber == 8 || currMonthNumber == 10) 
+    { //month with 30 days 
+        if (currDayNumber == 30) {
+            endOfMonth = true;
+        }
+        else { //redundant case but just in case
+            endOfMonth = false;
+        }
+    }
+    else 
+    { //february
+        if (isLeapYear == true && currDayNumber == 29) {
+            endOfMonth = true;
+        }
+        else if (isLeapYear == false && currDayNumber == 28) {
+            endOfMonth = true;
+        }
+        else { //redundant case but just in case
+            endOfMonth = false;
+        }
+    }
+    
+    //set day of week
+    if (hour <2) {currWeekdayNumber ++} //timezone danger zone
+    let currWeekdayENG;
+    let currWeekdayFR;
+
+    if(currWeekdayNumber==1) {
+        currWeekdayENG = 'Monday';
+        currWeekdayFR = "Lundi";
+    }
+    else if (currWeekdayNumber==2) {
+        currWeekdayENG = 'Tuesday';
+        currWeekdayFR = "Mardi";
+    }
+    else if (currWeekdayNumber==3) {
+        currWeekdayENG = 'Wednesday';
+        currWeekdayFR = "Mercredi";
+    }
+    else if (currWeekdayNumber==4) {
+        currWeekdayENG = 'Thursday';
+        currWeekdayFR = "Jeudi";
+    }
+    else if (currWeekdayNumber==5) {
+        currWeekdayENG = 'Friday';
+        currWeekdayFR = "Vendredi";
+    }
+    else if (currWeekdayNumber==6) {
+        currWeekdayENG = 'Saturday';
+        currWeekdayFR = "Samedi";
+    }
+    else {
+        currWeekdayENG = 'Sunday';
+        currWeekdayFR = "Dimanche";
+    }
+
+
+    // SET month
+    if(hour<2 && endOfMonth == true && currMonthNumber <=10) {
+        currMonthNumber ++;
+    } //timezone danger zone, increment month IFF at end of month and NOT december
+    else if (hour < 2 && endOfMonth == true && currMonthNumber == 11)
+    {
+        currMonthNumber =0;
+    } //timezone danger zone, increment month IFF at end OF december
+
+    let currMonthNameENG;
+    let currMonthNameFR;
+
+    if(currMonthNumber == 0) {
+        currMonthNameENG = 'January';
+        currMonthNameFR = "Janvier";
+    }
+    else if(currMonthNumber == 1) {
+        currMonthNameENG = 'February';
+        currMonthNameFR = "Fevrier";
+    }
+    else if(currMonthNumber == 2) {
+        currMonthNameENG = 'March';
+        currMonthNameFR = "Mars";
+    }
+    else if(currMonthNumber == 3) {
+        currMonthNameENG = 'April';
+        currMonthNameFR = "Avril";
+    }
+    else if(currMonthNumber == 4) {
+        currMonthNameENG = 'May';
+        currMonthNameFR = "Mai";
+    }
+    else if(currMonthNumber == 5) {
+        currMonthNameENG = 'June';
+        currMonthNameFR = "Juin";
+    }
+    else if(currMonthNumber == 6) {
+        currMonthNameENG = 'July';
+        currMonthNameFR = "Juillet";
+    }
+    else if(currMonthNumber == 7) {
+        currMonthNameENG = 'August';
+        currMonthNameFR = "Aout";
+    }
+    else if(currMonthNumber == 8) {
+        currMonthNameENG = 'September';
+        currMonthNameFR = "Septembre";
+    }
+    else if(currMonthNumber == 9) {
+        currMonthNameENG = 'October';
+        currMonthNameFR = "Octobre";
+    }
+    else if(currMonthNumber == 10) {
+        currMonthNameENG = 'November';
+        currMonthNameFR = "Novembre";
+    }
+    else {
+        currMonthNameENG = 'December';
+        currMonthNameFR = "Decembre";
+    }
+
+
+    //setting day of month
+    //TIMEZONE DANGER: DAY
+    if(hour<2 && endOfMonth == false) { //timezone danger zone, midmonth
+        currDayNumber ++;
+    } 
+    else if (hour < 2 && endOfMonth == true) {//timezone danger zone, end of month, wrap to first of month
+        currDayNumber = 1;
+    }
+
+    // Setting HTML!!!
+    weekdayENG.innerHTML = currWeekdayENG + ', ';
+    weekdayFR.innerHTML = currWeekdayFR + ', '
+    monthENG.innerHTML = currMonthNameENG + ' ';
+    monthFR.innerHTML = ' ' + currMonthNameFR;
+    dayENG.innerHTML = currDayNumber;
+    dayFR.innerHTML = currDayNumber;
+
+    //if festival time!
+    if (isLeapYear == true && currMonthNumber == 8 && currDayNumber < 23 && currDayNumber > 16) {
+
+    }
+    else if (isLeapYear == true && currMonthNumber == 8 && currDayNumber < 23 && currDayNumber > 17) {
+
+    }
+    else {
+        changeRevolutionLabels(currDayNumber, currMonthNumber, currYear, isLeapYear);
+    }
+
+}
+
+function changeRevolutionLabels(currDayNumber, currMonthNumber, currYear, isLeapYear) {
+    
+    /*
+        If normal day of year, NOT festival season at the end of the year
+        Set month, day, and weekday in French and English for revolutionary clock only
+    */
+
+    //necessary variables
+    let revMonthENG;
+    let revMonthFR;
+    let revWeekdayENG;
+    let revWeekdayFR;
+    let revDayENG;
+    let revDayFR;
+    let revSeason;
+    let revDayNumber; //xth day of the year (1-366)
+    let revDayNumberINMONTH; //xth day of the month (1-30)
+    
 
     { //CODE BLOCK FOR SETTING REVOLUTIONARY MONTH 
     if (currMonthNumber == 8) //september
@@ -448,64 +591,64 @@ function setRealDate() {
             revMonthENG = 'Eleventh Month'
         }
     }
-    //jesus that was exhausting and I already know theres no way its right
+    //jesus that was exhausting and I already know theres no way its right //IT WAS RIGHT LETS GOOOO
     }
 
-    { //CODE BLOCK FOR SETTING WEEKDAY
-        let revWeekdayNumber = revDayNumber%10;
-        if(revWeekdayNumber == 1) 
-        {
-            revWeekdayENG = 'First Weekday'
-            revWeekdayFR = 'Primidi'
-        }
-        else if (revWeekdayNumber == 2) 
-        {
-            revWeekdayENG = 'Second Weekday'
-            revWeekdayFR = 'Duodi'
-        }
-        else if (revWeekdayNumber == 3) 
-        {
-            revWeekdayENG = 'Third Weekday'
-            revWeekdayFR = 'Tridi'
-        }
-        else if (revWeekdayNumber == 4) 
-        {
-            revWeekdayENG = 'Fourth Weekday'
-            revWeekdayFR = 'Quartidi'
-        }
-        else if (revWeekdayNumber == 5) 
-        {
-            revWeekdayENG = 'Fifth Weekday'
-            revWeekdayFR = 'Quintidi'
-        }
-        else if (revWeekdayNumber == 6) 
-        {
-            revWeekdayENG = 'Sixth Weekday'
-            revWeekdayFR = 'Sextidi'
-        }
-        else if (revWeekdayNumber == 7) 
-        {
-            revWeekdayENG = 'Seventh Weekday'
-            revWeekdayFR = 'Octidi'
-        }
-        else if (revWeekdayNumber == 8) 
-        {
-            revWeekdayENG = 'Eighth Weekday'
-            revWeekdayFR = 'Nonidi'
-        }
-        else if (revWeekdayNumber == 9) 
-        {
-            revWeekdayENG = 'Ninth Weekday'
-            revWeekdayFR = 'Duodi'
-        }
-        else 
-        {
-            revWeekdayENG = 'Tenth Weekday'
-            revWeekdayFR = 'Decadi'
-        }
+    //SETTING WEEKDAY
+    let revWeekdayNumber = revDayNumber%10;
+    if(revWeekdayNumber == 1) 
+    {
+        revWeekdayENG = 'First Weekday'
+        revWeekdayFR = 'Primidi'
     }
+    else if (revWeekdayNumber == 2) 
+    {
+        revWeekdayENG = 'Second Weekday'
+        revWeekdayFR = 'Duodi'
+    }
+    else if (revWeekdayNumber == 3) 
+    {
+        revWeekdayENG = 'Third Weekday'
+        revWeekdayFR = 'Tridi'
+    }
+    else if (revWeekdayNumber == 4) 
+    {
+        revWeekdayENG = 'Fourth Weekday'
+        revWeekdayFR = 'Quartidi'
+    }
+    else if (revWeekdayNumber == 5) 
+    {
+        revWeekdayENG = 'Fifth Weekday'
+        revWeekdayFR = 'Quintidi'
+    }
+    else if (revWeekdayNumber == 6) 
+    {
+        revWeekdayENG = 'Sixth Weekday'
+        revWeekdayFR = 'Sextidi'
+    }
+    else if (revWeekdayNumber == 7) 
+    {
+        revWeekdayENG = 'Seventh Weekday'
+        revWeekdayFR = 'Septidi'
+    }
+    else if (revWeekdayNumber == 8) 
+    {
+        revWeekdayENG = 'Eighth Weekday'
+        revWeekdayFR = 'Octidi'
+    }
+    else if (revWeekdayNumber == 9) 
+    {
+        revWeekdayENG = 'Ninth Weekday'
+        revWeekdayFR = 'Nonidi'
+    }
+    else 
+    {
+        revWeekdayENG = 'Tenth Weekday'
+        revWeekdayFR = 'Decadi'
+    }
+    
 
-    revDayNumberINMONTH = revDayNumber%30;
+    revDayNumberINMONTH = revDayNumber%30; //very convenient system to be fair
 
     //HTML elements
     let revWeekdayElementFR = document.getElementById("weekday-past-fr");
@@ -519,74 +662,13 @@ function setRealDate() {
     revWeekdayElementFR.innerHTML = revWeekdayFR + ', ';
     revWeekdayElementENG.innerHTML = revWeekdayENG + ', ';
     revDayElementFR.innerHTML = revDayNumberINMONTH + ' ';
-    revDayElementENG.innerHTML = ' ' + revDayNumberINMONTH;
+    revDayElementENG.innerHTML = ' ' + revDayNumberINMONTH + 'th Day';
     revMonthElementFR.innerHTML = revMonthFR;
-    revMonthElementENG.innerHTML = revMonthENG;
-
+    revMonthElementENG.innerHTML = revMonthENG + ',';
 }
 
-function setRevDate() {
-    //WHAT TIME IS IT????
-    const now = new Date();
+setInterval(setRealTime, 1000);
+setInterval(setRevTime, 864);
 
-    //get seconds
-    const seconds = now.getSeconds();
-    //get minutes
-    const mins = now.getMinutes();
-    //get hours
-    const hour = now.getHours();
-
-    //SECONDS ELAPSED SINCE MIDNIGHT
-    let currTime = seconds + (60 * mins) + (3600 * hour);
-
-    //get hours
-    let percentage = currTime/86400;
-    let hourDeg2 = (360 * percentage) + 90;
-    let hours2 = 10*percentage;
-    //change hour hand
-    hourHand2.style.transform = `rotate(${hourDeg2}deg)`;
-    //update hour digital
-    let dig2Hour = document.getElementById('dig-2-hour')
-    if (hours2<10) {
-        dig2Hour.innerHTML = '0' + Math.floor(hours2);
-    }
-    else {
-        dig2Hour.innerHTML = Math.floor(hours2);
-    }
-
-    //get mins
-    let minPercentage = hours2%1;
-    let mins2 = minPercentage * 100;
-    let minDeg2 = (minPercentage * 360) + 90;
-    //update min hand
-    minsHand2.style.transform = `rotate(${minDeg2}deg)`;
-    //update min digital
-    let dig2Min = document.getElementById('dig-2-min')
-    if (mins2<10) {
-        dig2Min.innerHTML = '0' + Math.floor(mins2);
-    }
-    else {
-        dig2Min.innerHTML = Math.floor(mins2);
-    }
-
-    //get secs
-    let secPercentage = mins2%1;
-    let secs2 = secPercentage*100;
-    let secDeg2 = (secPercentage * 360) + 90;
-    //update sec hand
-    secondHand2.style.transform = `rotate(${secDeg2}deg)`;
-    //update sec digital
-    let dig2Sec = document.getElementById('dig-2-sec')
-    if (secs2<10) {
-        dig2Sec.innerHTML = '0' + Math.floor(secs2);
-    }
-    else {
-        dig2Sec.innerHTML = Math.floor(secs2);
-    }
-}
-
-setInterval(setRealDate, 1000);
-setInterval(setRevDate, 864);
-
-setRealDate();
-setRevDate();
+setRealTime();
+setRevTime();
